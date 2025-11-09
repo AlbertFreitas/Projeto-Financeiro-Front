5a1093385658d96bd12cd746a76055a721b47603
albert <albertdumont20@hotmail.com>
2025-11-09 19:42:18 -0300

    Adicao de Rotas Privadas

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+
+    return (
+        <div>
+            <h1 className="text-lg font-bold">Header</h1>
+        </div>
+    );
+}
+export default Header;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,9 @@ import Login from "../pages/Login";
 import  {AuthProvider } from "../context/AuthContext";
 import Dashboard from "../pages/Dashboard";
 
+import PrivateRoutes from "./PrivateRoutes";
+import AppLayout from "../layout/AppLayout";
+
 const AppRoutes = () => {
     return (
         <BrowserRouter>
@@ -11,7 +14,15 @@ const AppRoutes = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
+
+
+                    <Route element={<PrivateRoutes />}>
+                        <Route element={<AppLayout />}>
+                            <Route path="/dashboard" element={<Dashboard />} />
+                        </Route>
+                    </Route>
+
+
                     <Route path="*" element={<div>404 Not Found</div>} />
                 </Routes>
             </AuthProvider>
